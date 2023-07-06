6fff17c547c373caca53f9bdcb8fd5596394b5de
Devan Anjelito <anjelitodevan@gmail.com>
2023-07-06 15:44:28 +0700

    Change text with spinner.

--- main.py
+++ main.py
@@ -15,20 +15,20 @@ df_db = st.file_uploader("Upload DB sampai dengan hari sebelumnya disini")
 if (df_db is not None) and (df_daily is not None):
 
     # read files
-    data_load_state = st.text('Mohon tunggu, sedang mengupload...')
-    df_db = pd.read_excel(df_db)
-    df_daily = pd.read_excel(df_daily)
+    with st.spinner('Mohon tunggu, sedang mengupload...'):
+        df_db = pd.read_excel(df_db)
+        df_daily = pd.read_excel(df_daily)
 
-    # assert than DB is before daily
-    db_date= df_db['Tanggal'].max() - pd.Timedelta('1 minute')
-    daily_date= df_daily['Tanggal'].max()
-    if db_date > daily_date:
-        st.error('Tanggal daily lebih awal daripada tanggal DB', icon="🚨")
+        # assert than DB is before daily
+        db_date= df_db['Tanggal'].max() - pd.Timedelta('1 minute')
+        daily_date= df_daily['Tanggal'].max()
+        if db_date > daily_date:
+            st.error('Tanggal daily lebih awal daripada tanggal DB', icon="🚨")
 
     # do the calculation
-    data_load_state = st.text('Menghitung...')
-    df_daily_clean = module.clean_df_daily(df_daily, df_db)
-    summary = module.get_summary_per_day(df_daily_clean)
+    with st.spinner('Menghitung...'):
+        df_daily_clean = module.clean_df_daily(df_daily, df_db)
+        summary = module.get_summary_per_day(df_daily_clean)
     st.success('Perhitungan selesai!', icon="✅")
     
     # display result
@@ -37,26 +37,27 @@ if (df_db is not None) and (df_daily is not None):
 
     # download merged DB
     st.subheader('Langkah 3: Download Full Database')
-    data_load_state = st.text('Mempersiapkan database...')
-    cols= df_db.columns
-    df_daily_clean= df_daily_clean[cols]
-    df_merged= pd.concat([df_db, df_daily_clean], axis = 0)
-    date= df_daily_clean['Tanggal'].max().strftime('%d %b %Y')
-    filename= f'DB - {date}.xlsx'
 
-    # write dataframe
-    import io
-    buffer = io.BytesIO()
-    with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-        df_merged.to_excel(writer, sheet_name='Sheet1', index=False)
-        writer.close()
+    with st.spinner('Mempersiapkan database...'):
+        cols= df_db.columns
+        df_daily_clean= df_daily_clean[cols]
+        df_merged= pd.concat([df_db, df_daily_clean], axis = 0)
+        date= df_daily_clean['Tanggal'].max().strftime('%d %b %Y')
+        filename= f'DB - {date}.xlsx'
 
-        st.success('Download siap!', icon="✅")
-        
-        # render download button
-        st.download_button(
-            label= "Click Untuk Mendownload Full DB",
-            data= buffer,
-            file_name= filename,
-            mime= "application/vnd.ms-excel"
-        )
+        # write dataframe
+        import io
+        buffer = io.BytesIO()
+        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
+            df_merged.to_excel(writer, sheet_name='Sheet1', index=False)
+            writer.close()
+
+            st.success('Download siap!', icon="✅")
+            
+            # render download button
+            st.download_button(
+                label= "Click Untuk Mendownload Full DB",
+                data= buffer,
+                file_name= filename,
+                mime= "application/vnd.ms-excel"
+            )
